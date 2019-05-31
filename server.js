ac6ee2a3114bf1744f665d1c4d35418ed895b94c
CHinX2 <iris870425@gmail.com>
2019-05-31 23:50:09 +0800

    finish: patch

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ async function onGet(req, res) {
   var rcnt = Object.values(rows).length;
 
   // TODO(you): Finish onGet.
+  console.log("--GET--");
+
   for(var i = 1; i<rcnt; i++) {
     var val = Object.values(rows[i]);
     var item = {};
@@ -53,17 +55,19 @@ async function onPost(req, res) {
   var correspond = 0;
 
   // TODO(you): Implement onPost.
+  console.log("--POST--");
+
   for(var i=0; i<tcnt; i++) {
-    if(correspond === 1)break;
+    //if(correspond === 1)break;
     for(var j=0; j<pcnt; j++) {
       if(key[j].toLowerCase() === tag[i].toLowerCase()) {
-        for(var k=0; k<rcnt; k++) {
-          var val = Object.values(rows[k]);
-          if(val[i].toLowerCase() === value[j].toLowerCase()) {
-            correspond = 1;
-            break;
-          }
-        }
+        //for(var k=0; k<rcnt; k++) {
+        //  var val = Object.values(rows[k]);
+        //  if(val[i].toLowerCase() === value[j].toLowerCase()) {
+        //    correspond = 1;
+        //    break;
+        //  }
+        //}
         item[i] = value[j];
         check = check + 1;
       }
@@ -72,6 +76,7 @@ async function onPost(req, res) {
   //console.log(item);
   if(check === tcnt && correspond === 0) {
     var pro = await sheet.appendRow(item);
+    console.log("new: "+item);
   }
 
   res.json( { response: 'success'} );
@@ -82,14 +87,39 @@ async function onPatch(req, res) {
   const column  = req.params.column;
   const value  = req.params.value;
   const messageBody = req.body;
-  console.log(column);
-  console.log(value);
-  console.log(messageBody);
+  const result = await sheet.getRows();
+  const rows = result.rows;
+  const tag = Object.values(rows[0]);
+  var tcnt = tag.length;
+  var rcnt = Object.values(rows).length;
+  var nkey = Object.keys(messageBody);
+  var nval = Object.values(messageBody);
+  var hold = tcnt;
 
   // TODO(you): Implement onPatch.
-  
+  console.log("--PATCH--");
 
-  res.json( { status: 'unimplemented'} );
+  for(var j=0; j<tcnt; j++) {
+    if(tag[j].toLowerCase() === nkey[0].toLowerCase())
+      var cidx = j;
+    if(tag[j].toLowerCase() === column.toLowerCase() && hold === tcnt) {
+      hold = j;
+    }
+  }
+
+  for(var i = 1; i<rcnt; i++) {
+    if(hold === tcnt || cidx === undefined) break;
+    var val = Object.values(rows[i]);
+    //console.log(val+' '+value);
+    if(val[hold].toLowerCase() === value.toLowerCase()) {
+      val[cidx] = nval[0];
+      var pro = await sheet.setRow(i, val);
+      console.log("change: "+val);
+      break;
+    }
+  }
+
+  res.json( { response: 'success'} );
 }
 app.patch('/api/:column/:value', jsonParser, onPatch);
 
@@ -101,9 +131,11 @@ async function onDelete(req, res) {
   const tag = Object.values(rows[0]);
   var tcnt = tag.length;
   var rcnt = Object.values(rows).length;
+  var j=0;
 
   // TODO(you): Implement onDelete.
-  for(var j=0; j<tcnt; j++) {
+  console.log("--DELETE--");
+  for(j=0; j<tcnt; j++) {
       if(tag[j].toLowerCase() === column.toLowerCase())
         break;
   }
@@ -111,6 +143,8 @@ async function onDelete(req, res) {
     var val = Object.values(rows[i]);
     if(val[j].toLowerCase() === value.toLowerCase()) {
       var pro = await sheet.deleteRow(i);
+      console.log("del: "+val);
+      break;
     }
   }
 
